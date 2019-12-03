9616c35d3fb8208071a1db270630c089d99c59ab
Evan <evrussek@gmail.com>
2019-12-03 17:43:29 +0000

    ....

--- public/ver_4/index_scratch.js
+++ public/ver_4/index_scratch.js
@@ -9,122 +9,86 @@
     fullscreen_mode: true
   };
 
-  // we'll vary the travel amount?
-  var travel_amounts = [8, 16, 32];
-  var harvest_key_seq = ['f', 'j', 'f', 'j'];
-  var harvest_prompt = ["(F -> J -> F -> J)"];
+    // we'll vary the travel amount
+    var reward_amounts = [50, 75, 100];
+    var reward_names = ['LOW', 'MEDIUM', 'HIGH'];
+    var travel_names = ['EASY', 'HARD'];
+    var harvest_key_seq = ['u'];
+    var harvest_prompt = ['u'];
 
-  var travel_key_seq_easy = ['d', 'k', 'd', 'k'];
-  var travel_prompt_easy = ["(D -> K -> D -> K)"];
-  var travel_key_seq_hard =['z', '/', 't', 'y'];
-  var travel_prompt_hard = ["(Z -> / -> T -> Y)"];
-  var n_rounds = 2;
+    var travel_key_seq_hard = ['a'];
+    var travel_hold_down_keys_hard = ['0', '9', 'm','t','e'];
+    var travel_prompt_hard = ["Repeatedly press 'a' (left pinky) while holding down  't' , 'e' (left) and '0', '9', 'm' (right) to travel"];
+    var harvest_prompt_hard =  ["Repeatedly press 'u' to harvest or 'a' to travel"];
 
-  var total_points_arr = [];
+    var travel_hold_down_keys_easy = ['0', '9', 'm'];
+    var travel_key_seq_easy =['f'];
+    var travel_prompt_easy = ["Repeatedly press 'f' (left index) while holding down '0', '9', 'm' (right) to travel"];
+    var harvest_prompt_easy=  ["Repeatedly press 'u' to harvest or 'f' to travel"];
 
-  var forage_trials = [];
-  var trial_num = 0;
-  for (r_idx = 0; r_idx < n_rounds; r_idx++){
-    var this_round_trials = [];
-    for (d_idx = 0; d_idx < 2; d_idx++){
-      for (ta_idx = 0; ta_idx < travel_amounts.length; ta_idx++){
-        var this_travel_amount = travel_amounts[ta_idx];
-        if (d_idx == 0){
-            var this_travel_key_seq = travel_key_seq_easy;
-            var this_travel_prompt = travel_prompt_easy;
-        }else{
-          var this_travel_key_seq = travel_key_seq_hard;
-          var this_travel_prompt = travel_prompt_hard;
+    var n_rounds = 1;
+
+    var total_points_arr = [];
+
+    var forage_trials = [];
+    var trial_num = 0;
+    for (r_idx = 0; r_idx < n_rounds; r_idx++){
+      var this_round_trials = [];
+      for (d_idx = 0; d_idx < 2; d_idx++){ // key difficulty...
+        for (rew_idx = 0; rew_idx < reward_amounts.length; rew_idx++){ // travel index
+          var this_travel_amount = travel_amounts[ta_idx];
+          if (d_idx == 0){ // 0 is easy...
+              var this_travel_key_seq = travel_key_seq_easy;
+              var this_travel_prompt = travel_prompt_easy;
+              var this_travel_held_down_keys = travel_hold_down_keys_easy;
+              var this_harvest_prompt = harvest_prompt_easy;
+          }else{ // 1 is hard...
+            var this_travel_key_seq = travel_key_seq_hard;
+            var this_travel_prompt = travel_prompt_hard;
+            var this_travel_held_down_keys = travel_hold_down_keys_hard;
+            var this_travel_prompt = travel_prompt_hard;
+            var this_harvest_prompt = harvest_prompt_hard;
+          }
+          trial_num = trial_num + 1;
+          var this_trial = {
+            type: 'travel-mkre',
+            start_reward: reward_amounts[rew_idx],
+            decay: .98,
+            n_travel_steps: 16,
+            press_success_prob_travel: .8,
+            press_success_prob_harvest: .5,
+            reward_noise: 2.5,
+            start_reward_noise: 4,
+            time_min: 2.34,
+            travel_key_seq:this_travel_key_seq,
+            travel_prompt: this_travel_prompt,
+            harvest_key_seq: harvest_key_seq,
+            harvest_prompt: this_harvest_prompt,
+            travel_held_down_keys: this_travel_held_down_keys,
+            rew_idx: r_idx,
+            d_idx: d_idx,
+            reward_name: reward_names[rew_idx],
+            travel_name: travel_names[d_idx]
+          }
+          this_round_trials.push(this_trial)
         }
-        trial_num = trial_num + 1;
-        var this_trial = {
-          type: 'travel-mkre',
-          start_reward: 100,
-          decay: .98,
-          n_travel_steps: this_travel_amount,
-          press_success_prob_travel: 1,
-          press_success_prob_harvest: .5,
-          reward_noise: 2.5,
-          start_reward_noise: 4,
-          time_min: 1.5,
-          travel_key_seq:this_travel_key_seq,
-          travel_prompt: this_travel_prompt,
-          harvest_key_seq: harvest_key_seq,
-          harvest_prompt: harvest_prompt
-        }
-        this_round_trials.push(this_trial)
       }
+      forage_trials = forage_trials.concat(jsPsych.randomization.shuffle(this_round_trials,1));
     }
-    forage_trials = forage_trials.concat(jsPsych.randomization.shuffle(this_round_trials,1));
+
+
+
+
+  // add a trial num
+  for (var i = 0;i < forage_trials.length;  i++){
+    forage_trials[i].trial_num = i + 1;
   }
 
-// add a trial num
-for (var i = 0;i < forage_trials.length;  i++){
-  forage_trials[i].trial_num = i + 1;
-}
-
-// fix the quiz!!!!!!!!!!!!!!!!!!!!
-
-var task_name = 'foragetask';
-var run_name = 'run1';
-
-var this_trial = {
-  type: 'travel-mkre',
-  start_reward: 100,
-  decay: .98,
-  n_travel_steps: 7,
-  press_success_prob_travel: .75,
-  press_success_prob_harvest: .5,
-  reward_noise: 2.5,
-  start_reward_noise: 4,
-  time_min: .25,
-  travel_key_seq: ['a'],
-  travel_prompt: ["Repeatedly press 'a' while holding down  '0', '9', 'm' (right) and 't' , 'r' (left) to travel"],
-  harvest_key_seq: ['u'],
-  harvest_prompt:  ["Press 'u' to harvest or 'a' to travel"],
-  travel_held_down_keys: ['0', '9', 'm','t','r'], //  [85, 73, 79, 80, 78]
-  show_prompt: true
-}
-
-// add down a symbol if the keys are held down... -- repeated checks?
-
-var this_trial2 = { // this is the easy condition...
-  type: 'travel-mkre',
-  start_reward: 100,
-  decay: .98,
-  n_travel_steps: 7,
-  press_success_prob_travel: .8,
-  press_success_prob_harvest: .5,
-  reward_noise: 2.5,
-  start_reward_noise: 4,
-  time_min: .25,
-  travel_key_seq: ['f'],
-  travel_prompt:  ["Repeatedly press 'f' while holding down  '0', '9', 'm' (right) to travel"],
-  harvest_key_seq: ['u'],
-  harvest_prompt:  ["Press 'u' to harvest or 'f' to travel"],
-  travel_held_down_keys: ['0', '9', 'm'],
-  show_prompt: true
-}
-
-/*var this_trial2 = {
-  type: 'travel-mkre',
-  start_reward: 100,
-  decay: .98,
-  n_travel_steps: this_travel_amount,
-  press_success_prob_travel: .7,
-  press_success_prob_harvest: .5,
-  reward_noise: 2.5,
-  start_reward_noise: 4,
-  time_min: 1,
-  travel_key_seq: ['h', 'h', 'h', 'h','h', 'h', 'h', 'h', 'a', 'a', 'l', 'l', 'f', 'f'],
-  travel_prompt: ["h (x 8) -> a (x 2) -> l (x 2) -> f (x 2)"],
-  harvest_key_seq: ['j'],
-  harvest_prompt:  ['j'],
-  show_prompt: true
-} */
-
+  var task_name = 'foragetask';
 
 //db.collection("tasks").doc('meg_generalisation_8').collection('subjects').doc(uid).collection('trial_data').doc('trial_' + trial_data.trial_number.toString()).set({trial_data});
+var make_info_trial = function
+
 
   var make_text_trial = function(travel_prompt, harvest_prompt, trial_num){
     if (trial_num == 1){
@@ -196,6 +160,9 @@ var this_trial2 = { // this is the easy condition...
   }
   //timeline.push(end_screen)
 
+// for each trial - before it starts, we should have a screen where it chooses and environment - / then a screen where it translates that
+// after the trial ends, we should have a screen where they are told how many points they recieved...
+
   // recording data...
 
   // need a screen thanking them for the task and also figuring out the bonus... -- do this tonight
@@ -206,12 +173,9 @@ console.log(timeline)
 // timeline --
 
 var info_trial = {
-  type: 'evan-display-text',
-  line_1: "Traveling to a new environment",
-  line_2: travel_prompt + ". Press 'u' to harvest",
-  line_3: ""
+  type: 'evan-select-env'
 }
-
+timeline = [full_screen, info_trial];
 
   /* start the experiment */
   jsPsych.init({
--- public/ver_4/jspsych-6.0.5/evan_plugins/jspsych-evan-select-env.js
+++ public/ver_4/jspsych-6.0.5/evan_plugins/jspsych-evan-select-env.js
@@ -60,9 +60,61 @@ jsPsych.plugins["evan-select-env"] = (function() {
           .attr("x", 0).attr("y", 0).attr("width", par.w)
           .attr("height", par.h).style("fill", par.svg_color).style("opacity",.7);
 
-    place_text(trial.line_1, 'text', par.w/2, par.h/2 - par.text_font_size, par.text_font_size/2, 1, "White");
-    place_text(trial.line_2, 'text', par.w/2, par.h/2, par.text_font_size/2, 1, "White");
-    place_text(trial.line_3, 'text', par.w/2, par.h/2 + par.text_font_size, par.text_font_size/2, 1, "White");
+   place_text("Choosing a next environment...", "prompt", par.w/4, par.h/8, 45, 1, "White")
+   place_text("Tree point richness: ", "prompt", par.w/3, par.h/5, 35, 1, "blue")
+   place_text("Travel key press: ", "prompt", 1.75*par.w/3, par.h/5, 35, 1, "red")
+
+   var rect_height = par.h/8
+
+    // left rectangles
+
+    d3.select("svg").append("rect").attr("class", "HIGH")
+          .attr("x", par.w/2 - par.w/5).attr("y", par.h/4).attr("width", par.w/6)
+          .attr("height", rect_height).style("fill", 'blue').style("opacity",.025);
+
+   place_text("HIGH", "HIGH", par.w/3 + par.w/21, par.h/4 + rect_height/2 + 12, 35, .25, "white")
+
+    // show a rectangle
+    d3.select("svg").append("rect").attr("class", "MEDIUM")
+          .attr("x", par.w/2 - par.w/5).attr("y", par.h/4 + par.h/6).attr("width", par.w/6)
+          .attr("height", rect_height).style("fill", 'blue').style("opacity",.025);
+
+    place_text("MEDIUM", "MEDIUM", par.w/3 + par.w/21, par.h/4 + par.h/6 + rect_height/2 + 12, 35, .25, "white")
+
+    // show a rectangle
+    d3.select("svg").append("rect").attr("class", "LOW")
+          .attr("x", par.w/2 - par.w/5).attr("y", par.h/4 + 2*par.h/6 ).attr("width", par.w/6)
+          .attr("height", rect_height).style("fill", 'blue').style("opacity",.025);
+
+    place_text("LOW", "LOW", par.w/3 + par.w/21, par.h/4 + 2*par.h/6 + rect_height/2 + 12, 35, .25, "white")
+
+    // right rectangles
+    d3.select("svg").append("rect").attr("class", "EASY")
+          .attr("x", par.w/2 - par.w/6 + par.w/5).attr("y", par.h/3).attr("width", par.w/6)
+          .attr("height", rect_height).style("fill", 'red').style("opacity",.025);
+
+    place_text("EASY", "EASY", par.w/2 - par.w/6 + par.w/5+ par.w/12, par.h/3 + rect_height/2 + 12, 35, .25, "white")
+
+    // show a rectangle
+    d3.select("svg").append("rect").attr("class", "HARD")
+          .attr("x", par.w/2 - par.w/6 + par.w/5).attr("y", par.h/3 + par.h/6 ).attr("width", par.w/6)
+          .attr("height", rect_height).style("fill", 'red').style("opacity",.025);
+
+    place_text("HARD", "HARD",  par.w/2 - par.w/6 + par.w/5+ par.w/12, par.h/3 + par.h/6 + rect_height/2 + 12, 35, .25, "white")
+
+    wait_for_time(1000,function(){
+      d3.selectAll('.' + 'LOW').transition().style("opacity", 1).duration(1000)
+    })
+    wait_for_time(2000,function(){
+      d3.selectAll('.' + 'HARD').transition().style("opacity", 1).duration(1000)
+    }
+
+    // show the travel prompt -- to travel in this environment
+
+  );
+
+
+    // wait for about one second...
 
     ///////////////////////////////////////
 
@@ -74,7 +126,7 @@ jsPsych.plugins["evan-select-env"] = (function() {
 
     if (trial.wait_for_press){
       // set up max response time?
-      var txt_y =  .8*par.h;
+      var txt_y =  .9*par.h;
       var prompt = 'Press 4 to continue'
       place_text(prompt, "prompt", par.w/2, txt_y, 28, 1, "White")
 
@@ -88,9 +140,9 @@ jsPsych.plugins["evan-select-env"] = (function() {
         // data saving
         var trial_data = {
           // add time to this...
-          line_1: trial.line_1,
-          line_2: trial.line_2,
-          line_3: trial.line_3,
+          //line_1: trial.line_1,
+          //line_2: trial.line_2,
+          //line_3: trial.line_3,
           rt: response.rt
         };
 
@@ -113,7 +165,6 @@ jsPsych.plugins["evan-select-env"] = (function() {
     }else{
       wait_for_time(par.text_info_prac_time,function(){ d3.select('svg').remove(); jsPsych.finishTrial(trial_data);});
     }
-
 
   };
 
